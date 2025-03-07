81065fcc4012f4153d30d08a5b4f876fc9a62ac8
Fabio <Fabio@SB-1491297>
2025-03-07 11:34:53 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploEF/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace ExemploEF.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
---
+++ ExemploEF/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace ExemploEF.Models
+{
+    public class Cliente
+    {
+        public Guid ClienteID { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime Nascimento { get; set; }
+    }
+}
